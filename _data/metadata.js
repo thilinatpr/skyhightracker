eaf415a55b7745a287f9d398f81390da7e82f969
thilinatpr <48860868+thilinatpr@users.noreply.github.com>
2024-03-29 00:59:14 +0530

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Skyhigh Tracker: Track Crypto Your Way - Personalized Prices",
+	url: "https://skyhightracker.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Level up your crypto game with Skyhigh Tracker - a powerful Google Sheets add-on! Personalize your crypto tracking experience. Follow your favorite coins in real-time, directly within your secure Google Sheets. Set price alerts, analyze trends, and make informed investment decisions – all without leaving your familiar spreadsheet environment.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Pradeep Thilina ",
+		email: "patprathnayaka@gmail.com",
+		url: "https://skyhightracker.com/about-me/"
 	}
 }
